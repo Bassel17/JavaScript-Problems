34c7fa2b2564a0f74d162ba899ffbada2582466c
Bassel <basselkanso82@gmail.com>
2019-08-11 00:49:25 +0300

    refactoring a bit

--- Basic-Algorithm-Scripting/Longest-Word.js
+++ Basic-Algorithm-Scripting/Longest-Word.js
@@ -1,19 +1,25 @@
 function findLongestWordLength(str) {
     const Array = [...str];
+    const Maximum = longestWordOf(Array);
+    return Maximum;
+}
+
+function longestWordOf(Array) {
     Array.push(' ');
     let counter = 0;
-    let Maximum = 0;
-    for(let i=0;i<Array.length;i++){
-        if(Array[i]!==" "){
+    let maximum = 0;
+    for (let i = 0; i < Array.length; i++) {
+        if (Array[i] !== " ") {
             counter++;
-        }else{
-            if(counter > Maximum){
-                Maximum = counter;
+        }
+        else {
+            if (counter > maximum) {
+                maximum = counter;
             }
-            counter =0;
+            counter = 0;
         }
     }
-    return Maximum;
+    return maximum;
 }
   
 console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology"));
