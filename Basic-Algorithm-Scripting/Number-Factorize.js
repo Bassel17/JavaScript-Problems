ef7947b8fd2fb879c8b1984b253782d0b7fd3c08
Bassel <basselkanso82@gmail.com>
2019-08-10 23:25:09 +0300

    adding comments and refactoring a bit

--- Basic-Algorithm-Scripting/Number-Factorize.js
+++ Basic-Algorithm-Scripting/Number-Factorize.js
@@ -1,9 +1,7 @@
 function factorialize(number) {
-    if(number === 0){
-        return 1;
-    }
-    number *= factorialize(number-1);
+    if(number === 0) return 1;
+    number *= factorialize(number-1);//looping using recursion
     return number;
 }
-  
+//Example:
 console.log(factorialize(20));
