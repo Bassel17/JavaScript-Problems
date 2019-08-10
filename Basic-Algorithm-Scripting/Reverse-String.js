f57c9e9a4f1a510aa6bbf2439bd683a4c7e3e23d
Bassel <basselkanso82@gmail.com>
2019-08-10 23:02:19 +0300

    adding comments to explain some steps in the function

--- Basic-Algorithm-Scripting/Reverse-String.js
+++ Basic-Algorithm-Scripting/Reverse-String.js
@@ -1,10 +1,10 @@
 function reverseString(string) {
-    const arrayOfString = [...string];
-    const reversedArrayOfString = [];
-    arrayOfString.forEach((character)=>reversedArrayOfString.unshift(character));
-    string="";
-    reversedArrayOfString.forEach((character)=> string += character);
+    const arrayOfString = [...string];//spreading the string in to individual characters inside the array using the spread operator
+    const reversedArrayOfString = [];//array that will hold the characters in reversed order
+    arrayOfString.forEach((character)=>reversedArrayOfString.unshift(character));//looping through the array and adding the characters in reverse
+    string="";//emptying the string
+    reversedArrayOfString.forEach((character)=> string += character);//concatinating the characters to the string
     return string;
   }
-  
+  //example:
   console.log(reverseString("bassel")); 
