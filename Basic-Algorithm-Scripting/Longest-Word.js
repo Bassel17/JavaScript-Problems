2088107b67c72da0a37e0cb800e0757e15bec679
Bassel <basselkanso82@gmail.com>
2019-08-11 00:57:28 +0300

    add comments to explain steps of  the function

--- Basic-Algorithm-Scripting/Longest-Word.js
+++ Basic-Algorithm-Scripting/Longest-Word.js
@@ -1,25 +1,25 @@
 function findLongestWordLength(str) {
-    const Array = [...str];
-    const Maximum = longestWordLengthOf(Array);
-    return Maximum;
+    const ArrayOfString = [...str];
+    const LongestWordLength = longestWordLengthOf(ArrayOfString);//returns longest word length in an array
+    return LongestWordLength;
 }
 
 function longestWordLenghtOf(Array) {
     Array.push(' ');
     let counter = 0;
     let maximum = 0;
-    for (let i = 0; i < Array.length; i++) {
+    for (let i = 0; i < Array.length; i++) {//loop throught array
         if (Array[i] !== " ") {
-            counter++;
+            counter++;//count the number of characters in a word
         }
         else {
-            if (counter > maximum) {
-                maximum = counter;
+            if (counter > maximum) {//compare the number of character of a word to the maximum current value 
+                maximum = counter;//update maximum value to a longer value
             }
-            counter = 0;
+            counter = 0;//restart counter
         }
     }
     return maximum;
 }
-  
+//Example:
 console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology"));
