ff7fd799dcbf13a94f72e48e848ef62eb9aef834
Bassel <basselkanso82@gmail.com>
2019-08-10 19:51:14 +0300

    adding an example of using the function

--- Basic-Algorithm-Scripting/Convert-Celsius-To-Fahrenheit.js
+++ Basic-Algorithm-Scripting/Convert-Celsius-To-Fahrenheit.js
@@ -1,4 +1,10 @@
 function convertCelsiusToFahrenheit(celsiusDegree){
-    const fahrenheitDegree = (celsiusDegree * (9/5)) + 32;
+    const fahrenheitDegree = (celsiusDegree * (9/5)) + 32;//Equation that converts celsius to fahrenheit
     return fahrenheitDegree;
 }
+
+//Eaxmple:
+const celsiusDegree = 30;
+console.log(`The Temperature in celsius ${celsiusDegree}`);//logging the degree in celsius to the console
+const fahrenheitDegree = convertCelsiusToFahrenheit(celsiusDegree);
+console.log(`The Temperature in fahrenheit ${fahrenheitDegree}`);//logging the degree in fahrenheit to the console
