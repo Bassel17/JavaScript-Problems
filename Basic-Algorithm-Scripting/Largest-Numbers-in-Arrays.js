a37deebe3526338eeee08453b09c75e4f2f4de88
Bassel <basselkanso82@gmail.com>
2019-08-11 14:22:32 +0300

    refactoring the code

--- Basic-Algorithm-Scripting/Largest-Numbers-in-Arrays.js
+++ Basic-Algorithm-Scripting/Largest-Numbers-in-Arrays.js
@@ -1,15 +1,18 @@
-function largestNumbersInArray(array) {
+function largestNumbersInSubArray(array) {
     const largestNumbersArray=[];
     array.forEach(subArray => {
-    let largestNumber = subArray[0];
-    subArray.forEach(number=>{
-        if(number > largestNumber){
-            largestNumber = number;
-        }
-    });
-    largestNumbersArray.push(largestNumber);
+        let largestNumber=largestNumberInArray(subArray);
+        largestNumbersArray.push(largestNumber);
     });
     return largestNumbersArray;
 }
 
-console.log(largestNumbersInArray([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+function largestNumberInArray(array){
+    let largestNumber = array[0];
+    array.forEach(number =>{
+        largestNumber = number > largestNumber ? number : largestNumber;
+    });
+    return largestNumber;
+}
+//Example:
+console.log(largestNumbersInSubArray([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
