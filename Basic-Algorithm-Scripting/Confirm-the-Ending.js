ad85ad9648d25ebcec7e6d0b2d6ad5be5167b60c
Bassel <basselkanso82@gmail.com>
2019-08-11 16:04:35 +0300

    adding more examples

--- Basic-Algorithm-Scripting/Confirm-the-Ending.js
+++ Basic-Algorithm-Scripting/Confirm-the-Ending.js
@@ -3,4 +3,10 @@ function confirmEnding(string, target) {
 }
 
 //Examples:
-console.log(confirmEnding("Bastian", "n"));
+console.log(confirmEnding("Bastian", "n"));//should return true
+console.log(confirmEnding("Congratulation", "on"));//should return true
+console.log(confirmEnding("Connor", "n"));//should return false
+console.log(confirmEnding("He has to give me a new name", "name"));//should return true
+console.log(confirmEnding("Open sesame", "same"));//should return true
+console.log(confirmEnding("Open sesame", "game"));//should return false
+console.log(confirmEnding("Abstraction", "action"));//should return true
