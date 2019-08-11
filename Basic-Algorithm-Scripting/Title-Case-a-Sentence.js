b69ed3e8ba7e4d2b9456afa9c63328ae3cfb5cde
Bassel <basselkanso82@gmail.com>
2019-08-11 21:13:00 +0300

    refactoring code

--- Basic-Algorithm-Scripting/Title-Case-a-Sentence.js
+++ Basic-Algorithm-Scripting/Title-Case-a-Sentence.js
@@ -1,15 +1,26 @@
 function titleCase(str) {
     const arrayOfWords = str.split(" ");
-    let newArray = []
-    arrayOfWords.forEach((word)=>{
-        let newWord=word.charAt(0).toUpperCase();
-        for(let i=1;i<word.length;i++){
-        newWord += word.charAt(i).toLowerCase();
-    }
-        newArray.push(newWord);
-    });
-    str = newArray.join(" ");
+    const arrayOfTitleCasedWords = titleCaseWordsInArray(arrayOfWords);
+    str = arrayOfTitleCasedWords.join(" ");
     return str;
-  }
-  
+}
+
+function titleCaseWordsInArray(arrayOfWords){
+    const arrayOfTitleCasedWords = [];
+    arrayOfWords.forEach((word)=>{
+        let titleCasedWord = titleCaseWord(word);
+        arrayOfTitleCasedWords.push(titleCasedWord);
+    });
+    return arrayOfTitleCasedWords;
+}
+
+function titleCaseWord(word){
+    let titleCasedWord = word.charAt(0).toUpperCase();
+    for(let i=1;i<word.length;i++){
+        titleCasedWord += word.charAt(i).toLowerCase();
+    }
+    return titleCasedWord;
+}
+
+//Example:
 console.log(titleCase("I'm a little tea pot"));
