8b8fd60df93db95e2eae2fa0c44f18a8cb66d735
Bassel <basselkanso82@gmail.com>
2019-08-15 21:28:42 +0300

    adding examples

--- Algorithms-and-DataStructures/Palindrome-Checker.js
+++ Algorithms-and-DataStructures/Palindrome-Checker.js
@@ -23,5 +23,16 @@ function areEqual(string1,string2){
         return false;
     }
 }
-  
-console.log(isPalindrome("_eye"));
+//Examples:
+console.log(isPalindrome("eye"));//Should return true
+console.log(isPalindrome("_eye"));//Should return true
+console.log(isPalindrome("race car"));//Should return true
+console.log(isPalindrome("not a palindrome"));//Should return false
+console.log(isPalindrome("A man, a plan, a canal. Panama"));//Should return true
+console.log(isPalindrome("never odd or even"));//Should return true
+console.log(isPalindrome("nope"));//Should return false
+console.log(isPalindrome("almostomla"));//Should return false
+console.log(isPalindrome("My age is 0, 0 si ega ym."));//Should return true
+console.log(isPalindrome("1 eye for of 1 eye."));//Should return false
+console.log(isPalindrome("0_0 (: /-\ :) 0-0"));//Should return true
+console.log(isPalindrome("five|\_/|four"));//Should return false
