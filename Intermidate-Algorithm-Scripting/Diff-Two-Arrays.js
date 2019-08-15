321c61cdc45b38bafde7c3b7154114ab55b1ccee
Bassel <basselkanso82@gmail.com>
2019-08-15 13:14:38 +0300

    created function diffArray

--- Intermidate-Algorithm-Scripting/Diff-Two-Arrays.js
+++ Intermidate-Algorithm-Scripting/Diff-Two-Arrays.js
@@ -1,6 +1,16 @@
 function diffArray(arr1, arr2) {
     var newArr = [];
+    arr1.forEach(element => {
+        if(arr2.indexOf(element) === -1){
+            newArr.push(element);
+        }
+    });
+    arr2.forEach(element => {
+        if(arr1.indexOf(element) === -1){
+            newArr.push(element);
+        }
+    })
     return newArr;
 }
   
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
