62db1019ca415fd9ebf70ff30c29fb6c7602b637
Bassel <basselkanso82@gmail.com>
2019-08-15 21:21:07 +0300

    refactoring code

--- Algorithms-and-DataStructures/Palindrome-Checker.js
+++ Algorithms-and-DataStructures/Palindrome-Checker.js
@@ -1,21 +1,27 @@
 function isPalindrome(str) {
     let lowerCasedStr = str.toLowerCase();
-    console.log(lowerCasedStr);
     let arrayOfCharacters = lowerCasedStr.split("");
     let arrayOfAlphaNumericCharacters = [];
-    for(i=0;i<arrayOfCharacters.length;i++){
-        if(arrayOfCharacters[i].match(/^[0-9a-z]+$/)){
-            arrayOfAlphaNumericCharacters.push(arrayOfCharacters[i]);
-        }
-    }
-    console.log(arrayOfAlphaNumericCharacters);
+    filterAlphaNumericCharacters(arrayOfCharacters, arrayOfAlphaNumericCharacters);
     let strOfAlphaNumericCharacters = arrayOfAlphaNumericCharacters.join("");
     let reversedStrOfAlphaNumericCharacters = arrayOfAlphaNumericCharacters.reverse().join("");
-    if(strOfAlphaNumericCharacters === reversedStrOfAlphaNumericCharacters){
+    return areEqual(strOfAlphaNumericCharacters,reversedStrOfAlphaNumericCharacters);
+}
+
+function filterAlphaNumericCharacters(arrayOfCharacters, arrayOfAlphaNumericCharacters) {
+    arrayOfCharacters.forEach((character)=>{
+        if (character.match(/^[0-9a-z]+$/)) {
+            arrayOfAlphaNumericCharacters.push(character);
+        }
+    });
+}
+
+function areEqual(string1,string2){
+    if(string1 === string2){
         return true
+    }else{
+        return false;
     }
-    return false;
 }
   
-  
-console.log(palindrome("_eye"));
+console.log(isPalindrome("_eye"));
