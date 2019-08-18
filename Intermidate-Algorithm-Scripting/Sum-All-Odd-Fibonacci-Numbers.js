c72d45eafa6105f7e3748ce2330205a6396fedc7
Bassel <basselkanso82@gmail.com>
2019-08-18 16:27:37 +0300

    created function that returns fibonacci series

---
+++ Intermidate-Algorithm-Scripting/Sum-All-Odd-Fibonacci-Numbers.js
@@ -0,0 +1,16 @@
+function fibonacciSeries(limit){
+    let currentNumber = 1;
+    let nextNumber = 1;
+    let sum;
+    const arrayOfFibonacciNumbers = [1,1];
+    for (let i=nextNumber;i < limit-1; i++){
+        sum = currentNumber + nextNumber;
+        currentNumber = nextNumber;
+        nextNumber = sum;
+        arrayOfFibonacciNumbers.push(nextNumber);
+    }
+    return arrayOfFibonacciNumbers;
+}
+
+
+console.log(fibonacciSeries(4));
