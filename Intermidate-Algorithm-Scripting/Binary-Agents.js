bbf7f128939fa4f2bef30f72ba604fb145411cd8
Bassel <basselkanso82@gmail.com>
2019-08-19 13:20:26 +0300

    refactored code for Bianry agent ptoblem

--- Intermidate-Algorithm-Scripting/Binary-Agents.js
+++ Intermidate-Algorithm-Scripting/Binary-Agents.js
@@ -2,10 +2,15 @@ function convertFromBinaryToCharacters(str) {
     const arrayOfBinaryNumbers = str.split(' ');
     const arrayOfCharacters = [];
     for (let i=0 ; i<arrayOfBinaryNumbers.length;i++){
-        arrayOfCharacters.push(String.fromCharCode(parseInt(arrayOfBinaryNumbers[i],2)));
+        arrayOfCharacters.push(convertFromBinaryToACharacter(arrayOfBinaryNumbers[i]));
     }
     str = arrayOfCharacters.join('');
     return str;
+}
+function convertFromBinaryToACharacter(binaryString){
+    const hexaDecimal = parseInt(binaryString,2);
+    const character = String.fromCharCode(hexaDecimal);
+    return character;
 }
 //Example:
 console.log(convertFromBinaryToCharacters("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
